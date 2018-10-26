2f41a3db95bacf08d30eea9777dbc5b8050da7d9
James Osborne <james.osborne112@gmail.com>
2018-10-26 11:20:38 -0400

    Add reverseString() function

--- src/kernel/util.c
+++ src/kernel/util.c
@@ -17,9 +17,21 @@ void intToString(int number, char outputString[]) {
     do {
         outputString[i++] = number % 10 + '0';
     } while ((number /= 10) > 0);
-
+    
     if (sign < 0) {
         outputString[i++] = '-';
     }
+    reverseString(outputString, i);
+
     outputString[i] = '\0';
+}
+
+void reverseString(char stringToReverse[], int length) {
+    int i;
+    for (i = 0; i < length / 2; ++i) {
+        char temp = stringToReverse[i];
+        int swapPosition = length - i - 1;
+        stringToReverse[i] = stringToReverse[swapPosition];
+        stringToReverse[swapPosition] = temp;
+    }
 }
--- src/kernel/util.h
+++ src/kernel/util.h
@@ -1,2 +1,3 @@
 void memoryCopy(char *source, char *dest, int numberOfBytes);
 void intToString(int number, char outputString[]);
+void reverseString(char stringToReverse[], int length);
