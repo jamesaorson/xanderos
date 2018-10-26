81150b8c48807087d5d3c6964d0dbc4d71966534
James Osborne <james.osborne112@gmail.com>
2018-10-26 11:23:12 -0400

    Add include guard

--- src/kernel/util.h
+++ src/kernel/util.h
@@ -1,3 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 void memoryCopy(char *source, char *dest, int numberOfBytes);
 void intToString(int number, char outputString[]);
 void reverseString(char stringToReverse[], int length);
+
+#endif
